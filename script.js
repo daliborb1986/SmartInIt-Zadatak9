92835c6ea6b66e43815aeff399e90448921ae775
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-05-28 23:53:25 +0200

    Completed start page with JS

--- script.js
+++ script.js
@@ -12,13 +12,21 @@ function checkFirstName() {
 let titleUpperCase = document.getElementById('firstPageTitle');
 
 titleUpperCase.style.textTransform = 'uppercase';
-titleUpperCase.style.fontWeight = "bold"
+titleUpperCase.style.fontWeight = 'bold';
 
 let titleHeader = document.getElementById('titleHeader');
 titleHeader.style.fontStyle = 'italic';
-titleHeader.style.fontWeight = "bold"
+titleHeader.style.fontWeight = 'bold';
 
+const page1 = document.getElementById('page1');
+const startPage = document.getElementById('startPage');
 const btnStart = document.getElementById('startButton');
 
+function changePage() {
+    startPage.classList.toggle('d-none');
+    if(page1.classList.contains('d-none')){
+        page1.classList.toggle('d-none');
+    }
+}
 
-
+btnStart.addEventListener('click', changePage);
