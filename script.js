da0e87b777a0219656c350a7d0bfcd79f628e4e4
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-05-29 11:31:31 +0200

    Finished JS for name input

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
+//  -------Buttons------//
+
 const btnNext = document.getElementById('nextButton');
+const page1 = document.getElementById('page1');
+const page2 = document.getElementById('page2');
+const page3 = document.getElementById('page3');
+const page4 = document.getElementById('page4');
+const startPage = document.getElementById('startPage');
+const btnStart = document.getElementById('startButton');
+const userNote = document.getElementById('noteToUser');
+var titleHeader = document.getElementById('titleHeader');
+var titleUpperCase = document.getElementById('firstPageTitle');
 
-btnNext.addEventListener('click', checkFirstName);
-
-function checkFirstName() {
-  const firstName = document.getElementById('name').value;
-
-  if (!firstName) {
-    alert('Name must be filled out');
-  }
-}
-let titleUpperCase = document.getElementById('firstPageTitle');
+// ----- CSS style ----- //
 
 titleUpperCase.style.textTransform = 'uppercase';
 titleUpperCase.style.fontWeight = 'bold';
 
-let titleHeader = document.getElementById('titleHeader');
 titleHeader.style.fontStyle = 'italic';
 titleHeader.style.fontWeight = 'bold';
+userNote.classList.add('noteToUser');
 
-const page1 = document.getElementById('page1');
-const startPage = document.getElementById('startPage');
-const btnStart = document.getElementById('startButton');
-
-function changePage() {
-    startPage.classList.toggle('d-none');
-    if(page1.classList.contains('d-none')){
-        page1.classList.toggle('d-none');
-    }
-}
+// ----Event listeners ---- //
 
 btnStart.addEventListener('click', changePage);
+btnNext.addEventListener('click', checkFirstName);
+btnNext.addEventListener('click', nextPageWithName);
 
-const userNote = document.getElementById('noteToUser');
+// ---- Functions ----- //
 
-userNote.classList.add("noteToUser");
-
-const userName = document.getElementById("name").value;
+var firstName = document.getElementById('name');
+function checkFirstName() {
+  if (!firstName.value) {
+    alert('Name must be filled out');
+  }
+}
+function changePage() {
+  startPage.classList.toggle('d-none');
+  if (page1.classList.contains('d-none')) {
+    page1.classList.toggle('d-none');
+  }
+}
+function nextPageWithName() {
+  if (firstName.value) {
+    page1.classList.toggle('d-none');
+    page2.classList.toggle('d-none');
+  }
+}
